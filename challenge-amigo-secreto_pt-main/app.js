8f9457c2f5cc3ce4cdc9280fb01747e8d61dc843
Paulo Ricardo <paulo.ricardo_silva@hotmail.com>
2025-02-04 02:19:28 -0300

    adicionado ifs

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -10,14 +10,24 @@ let ListaDeAmigosSorteados = [];
 
 
 function adicionarAmigo(){
+
+
     let texto = document.querySelector('#amigo').value
-    ListaDeAmigosSorteados.push(texto);
+  
+    
 
    lista = document.getElementsByTagName('input')
 
    var listagem = document.getElementById('listaAmigos')
    var listaOrdenada = document.createElement('ul');
+
+
     for(i = 0; i< lista.length; i++){
+        if(texto == ""){
+            alert("o campo nome esta vazio, insira um nome valido")
+            i=0;
+           }else{
+            ListaDeAmigosSorteados.push(texto);
         var novoItem = document.createElement('li');
         novoItem.innerHTML = lista[i].value;
         
@@ -29,10 +39,15 @@ function adicionarAmigo(){
     listagem.appendChild(listaOrdenada);
     console.log(texto);
 }
-
+}
 function sortearAmigo(){
+
     amigoSecreto = ListaDeAmigosSorteados[Math.floor(Math.random()*ListaDeAmigosSorteados.length)]
+    if(ListaDeAmigosSorteados == ""){
+        alert("antes digite um nome válido")
+    }else{
     alert(amigoSecreto) 
+}
 }
 
 
